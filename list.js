c7e89037243482c97f1188a85d1264b0dd2c3651
devarajvarsha <167671388+devarajvarsha@users.noreply.github.com>
2024-08-19 04:27:30 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,60 @@
+let listContainer = document.getElementById('list-container');
+let head = document.querySelector('head');
+let inputBox = document.getElementById('input-box');
+
+function addTask(){
+    if(inputBox.value == ''){
+        alert('add something to your list');
+    }else{
+        const task = document.createElement('li');
+        task.textContent = inputBox.value;
+        listContainer.appendChild(task);
+        let span = document.createElement('span');
+        span.textContent = "\u00d7";
+        task.appendChild(span);
+        span.style.right = '0px';
+    }
+
+
+
+
+    inputBox.value = '';
+    saveData();
+}
+
+
+listContainer.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+
+
+
